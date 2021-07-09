225999ae98b43f0f9a94bbff480be381a7eda2f7
troymeeker <troymeeker1@gmail.com>
2021-07-08 17:04:39 -0700

    completed

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (two,one) => {
+    return  (2000 / 100);
+}
+divide;
+
+const square = (num)=>{
+ return num*num;
+}
+square;
+
+const add = (one, six)=>{
+    return one+six;
+    
+}
+add(1,6);
